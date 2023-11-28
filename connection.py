5624d401b72c7848c56000818b6f3a1bfa65608d
ori-30 <99084444+ori-30@users.noreply.github.com>
2023-11-28 13:22:13 +0100

    chatgpt api

--- connection.py
+++ connection.py
@@ -3,90 +3,64 @@ from streamlit_chat import message
 import streamlit.components.v1 as components  # Import Streamlit
 import requests
 import json
-
+from openai import OpenAI
+from typing import List
+from functions import *
 st.set_page_config(
-    page_title="GenAI dashboards",
+    page_title="GenAI domains",
     page_icon=":heart:"
 )
 
+
+#abrir openAI key con streamlit secrets
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+#crear modelo por defecto
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+#inizializar chat
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+#creamos la sidebar
 with st.sidebar:
+	try:
+		st.header("Chatbot", divider='rainbow')
+		prompt = get_text()
+		#mostramos el chat de mensajes desde el historial
+		for message in st.session_state.messages:
+		    with st.chat_message(message["role"]):
+		        st.markdown(message["content"])
 
-	st.header("Chatbot")
-	#st.markdown("dashboards")
+		# Accept user input
+		if prompt:
+		    # Add user message to chat history
+		    st.session_state.messages.append({"role": "user", "content": prompt})
+		    # Display user message in chat message container
+		    with st.chat_message("user"):
+		        st.markdown(prompt)
+		    # Display assistant response in chat message container
+		    with st.chat_message("assistant"):
+		        message_placeholder = st.empty()
+		        full_response = ""
+		cl=client.chat.completions.create(model=st.session_state["openai_model"], messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages], stream=True)
+		for response in cl:
+			full_response +=(response.choices[0].delta.content or "")
+			message_placeholder.markdown(full_response + "▌")
+		message_placeholder.markdown(full_response)
+		st.session_state.messages.append({"role": "assistant", "content": full_response})
+	except:
+		""        
 
-	if 'generated' not in st.session_state:
-	    st.session_state['generated'] = []
-
-	if 'past' not in st.session_state:
-	    st.session_state['past'] = []
-
-
-	def get_text():
-	    input_text = st.text_input("You: ","", key="input")
-	    return input_text 
-
-
-	user_input = get_text()
-
-	if user_input:
-	    output = "Esto es una respuesta"
-	    st.session_state.past.append(user_input)
-	    st.session_state.generated.append(output)
-
-	if st.session_state['generated']:
-
-	    for i in range(len(st.session_state['generated'])-1, -1, -1):
-	        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-	        message(st.session_state["generated"][i], key=str(i))
 
 container= st.container()
-
-def get_JSON():
-	dominios ='{"Dominios": [{ "titulo": "Dominio 1", "tablas": ["tabla1","tabla2"] }, { "titulo": "Dominio 2", "tablas": ["tabla3","tabla4"] }, { "titulo": "Dominio 3", "tablas": ["tabla1","tabla2"] }, { "titulo": "Dominio 4", "tablas": ["tabla1","tabla2"] }]}'
-	return json.loads(dominios)
-
-def tables(alltables):
-	r=""
-	for table in alltables:
-		r+= """<p class="card-text">%s</p>""" % str(table)
-	return r
-def create_card(title, alltables):
-
-	card="""
-			<div class="card m-2" style="width: 18rem;">
-			  <div class="card-body bg-light">
-			    <h3 class="card-title">Dominio:%s</h3>
-	""" % str(title)
-	card+=tables(alltables)
-
-	card+=""" 			  
-				</div>
-			</div>
-		"""
-	st.markdown(card, unsafe_allow_html= True)
-
-def create_domains(dominios):
-	c0, c1, c2= container.columns(3)
-	i=0
-	for dominio in dominios:
-		if i==0:
-			with c0:
-				create_card(dominio["titulo"], dominio["tablas"])
-		if i==1:
-			with c1:
-				create_card(dominio["titulo"], dominio["tablas"])
-		if i==2:
-			with c2:
-				create_card(dominio["titulo"], dominio["tablas"])
-		i=(++i)%3
-
-
-
 with container:
-	_bootstrap="""<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">"""
-	st.markdown(_bootstrap, unsafe_allow_html= True)
+	bootstrap()
+	create_sql_statment(container)
 	dominios=get_JSON()
-	create_domains(dominios["Dominios"])
+	create_domains(dominios["Dominios"], container)
 
 		
 
